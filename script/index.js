10f3a6b0c8c805dda75a34eeff155e145667a209
Ntare Guy <gntare2@gmail.com>
2022-02-03 17:20:25 +0200

    Added speaker dynamically

---
+++ script/index.js
@@ -0,0 +1,51 @@
+const speaker = [
+  {
+    image: './images/pic.png',
+    sName: 'Ntare G',
+    sLocation: 'Rwanda, Kigali',
+    sBio: 'A associate Software developer at microverse with  expertise in Full-stack development and DevOps.',
+  },
+  {
+    image: './images/pic.png',
+    sName: 'Ntare Guy',
+    sLocation: 'Rwanda, Kigali',
+    sBio: 'A associate Software developer at microverse with  expertise in Full-stack development and DevOps.',
+  },
+  {
+    image: './images/pic.png',
+    sName: 'Ntare Guy',
+    sLocation: 'Rwanda, Kigali',
+    sBio: 'A associate Software developer at microverse with  expertise in Full-stack development and DevOps.',
+  },
+  {
+    image: './images/pic.png',
+    sName: 'Ntare Guy',
+    sLocation: 'Rwanda, Kigali',
+    sBio: 'A associate Software developer at microverse with  expertise in Full-stack development and DevOps.',
+  },
+];
+const speakerClass = document.querySelector('#speaker-id');
+
+  for(let i=0; i<=speaker.length; i++){
+    
+  speakerClass.innerHTML +=`
+    <section class="speaker-card">
+                <div class="speaker-content">
+                    <img src='${speaker[i].image}' width='150' alt="">
+                    <div class="speaker-desc">
+                        <h4>${speaker[i].sName}</h4>
+                        <h5>${speaker[i].sLocation}</h5>
+                        <hr>
+                        <p>${speaker[i].sBio}</p>
+                    </div>
+                </div>
+      </section>
+    `
+  }
+
+
+
+
+  
+  
+
